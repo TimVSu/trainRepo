2b9b41f6319e8e72f68cbff2494c8c63483d227b
Tim Suchan <tsuchan@smail.uni-koeln.de>
2023-01-08 21:47:02 +0100

    showcase merge

--- main.java
+++ main.java
@@ -2,5 +2,6 @@ public class main{
 	public static void main(String[] args){
 		System.out.println("Code at line 4");
 		System.out.println("Code at line 5, added from master");
+		System.out.println("Code at line 6, added to show merge cause i forgot ss");
 		}
 }
